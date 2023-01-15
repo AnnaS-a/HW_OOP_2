2499e79623d2f9d28e67939f032cab9de47012fc
anna <slukin1@yandex.ru>
2023-01-15 22:51:56 +0700

    Horse

---
+++ Horse.java
@@ -0,0 +1,21 @@
+public class Horse extends Animal implements Runable{
+
+    public Horse(String name, int box) {
+        super(name, box);
+    }
+
+    @Override
+    public String say() {
+        return "Иго-го";
+    }
+
+    @Override
+    public String toString() {
+        return "Horse " + super.toString();
+    }
+
+    @Override
+    public int speedOfRun() {
+        return 60;
+    }
+}
